039a3fcaa76ab9b8f4a8d196e1fb3671e21880aa
yaroslav <yarik545585@gmail.com>
2023-11-22 21:26:34 +0500

    test commit

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as express from 'express';
 import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
-
+  console.log('test')
   app.enableCors({ credentials: true, origin: true });
 
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
